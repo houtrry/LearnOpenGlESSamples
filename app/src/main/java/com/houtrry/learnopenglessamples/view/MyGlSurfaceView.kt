e2b15e623913a87792501e82f65be1c6354e8e0d
huangdejun <huangdejun@pudutech.com>
2023-04-15 18:57:32 +0800

    增加矩阵转换，opengl坐标转成屏幕坐标

--- app/src/main/java/com/houtrry/learnopenglessamples/view/MyGlSurfaceView.kt
+++ app/src/main/java/com/houtrry/learnopenglessamples/view/MyGlSurfaceView.kt
@@ -3,16 +3,16 @@ package com.houtrry.learnopenglessamples.view
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
-import com.houtrry.learnopenglessamples.opengl.SampleRender1
+import com.houtrry.learnopenglessamples.opengl.SampleRender2
 
 class MyGlSurfaceView(context: Context, attrs: AttributeSet? = null) :
     GLSurfaceView(context, attrs) {
 
-    private val simpleRender: SampleRender1
+    private val simpleRender: SampleRender2
 
     init {
         setEGLContextClientVersion(2)
-        simpleRender = SampleRender1(context)
+        simpleRender = SampleRender2(context)
         setRenderer(simpleRender)
     }
 }
