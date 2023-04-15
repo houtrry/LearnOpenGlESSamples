6ae4631435bea80f9ab38790f8687858a6e2fde8
huangdejun <huangdejun@pudutech.com>
2023-04-15 19:34:07 +0800

    增加z、w，变三维

--- app/src/main/java/com/houtrry/learnopenglessamples/view/MyGlSurfaceView.kt
+++ app/src/main/java/com/houtrry/learnopenglessamples/view/MyGlSurfaceView.kt
@@ -3,16 +3,16 @@ package com.houtrry.learnopenglessamples.view
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.util.AttributeSet
-import com.houtrry.learnopenglessamples.opengl.SampleRender2
+import com.houtrry.learnopenglessamples.opengl.SampleRender3
 
 class MyGlSurfaceView(context: Context, attrs: AttributeSet? = null) :
     GLSurfaceView(context, attrs) {
 
-    private val simpleRender: SampleRender2
+    private val simpleRender: SampleRender3
 
     init {
         setEGLContextClientVersion(2)
-        simpleRender = SampleRender2(context)
+        simpleRender = SampleRender3(context)
         setRenderer(simpleRender)
     }
 }
